6940ad4e326d97e234a13c498f521c834a5b3137
Cole C Dunnam <cdunna3@lsu.edu>
2019-03-26 11:37:45 -0500

    Update

---
+++ map2/webmap2.js
@@ -0,0 +1,11 @@
+let WorldImageMap = L.map('map2').setView([40.18, -99.14], 3.5)
+//L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(WorldImageMap)
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(WorldImageMap);
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(WorldImageMap)
