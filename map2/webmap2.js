11118ed51de01a7489bd503f0b09a9fa3b9adfa0
ccdunnam <you@example.com>
2019-04-07 20:21:47 -0500

    comit

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -3,21 +3,9 @@ let WorldImageMap = L.map('map2').setView([40.18, -99.14], 3.5)
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(WorldImageMap);
-// L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-//   layers: 'nexrad-n0r-900913',
-//   format: 'image/png',
-//   transparent: true,
-//   attribution: 'NOAA, Iowa State University'
-// }).addTo(WorldImageMap)
-// L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-//   layers: 'nexrad-n0r-900913',
-//   format: 'image/png',
-//   transparent: true,
-//   attribution: 'NOAA, Iowa State University'
-// }).addTo(WorldImageMap)
-// L.esri.dynamicMapLayer({
-//   url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
-// }).addTo(WorldImageMap)
 L.esri.featureLayer({
 	url: 'https://services9.arcgis.com/SDQDNhpG8jikA0D1/arcgis/rest/services/Top_50_US_Cities_List/FeatureServer/0'
+}).addTo(WorldImageMap)
+L.esri.featureLayer({
+	url: 'https://services9.arcgis.com/SDQDNhpG8jikA0D1/arcgis/rest/services/Higher_Education_US_2010_2014/FeatureServer/0'
 }).addTo(WorldImageMap)
